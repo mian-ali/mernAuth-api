35e15679ff795c809067ca29784aec22267cf62a
Ali Ahmad <mian41935@gmail.com>
2023-05-08 02:14:09 +0500

    Applying Third party middlewear ✔⭐ #3

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 const express = require('express');
-
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 // import routes
 const authRoutes = require('./routes/auth');
 const app = express();
+
+// application based middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+dotenv.config();
+
+app.use(cors()); // allows all origins
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `http://localhost:3000` }));
+}
 
 // middleware
 app.use('/api', authRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello Server');
+  res.send('Hello from nodejs server!');
 });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(
+    `Server is running on port ${port} -mode- ${process.env.NODE_ENV}`
+  );
 });
