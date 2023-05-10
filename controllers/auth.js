eeaeaf36e67693f6428d8a32914b5bea55cce756
Ali Ahmad <mian41935@gmail.com>
2023-05-10 17:43:25 +0500

    Express Validators Applyed for user Auth ⭐

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 // SIGNUP CONTROLLER
 exports.signup = (req, res) => {
+  console.log('REQ BODY ON SIGNUP', req.body);
   res.json({
     data: 'You hit singup endpoint controller!',
   });
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
-//import controller
-const { signup } = require('../controllers/auth');
+const { signup } = require('../controllers/auth');  // import signup controller
+const { userSignupValidator } = require('../validators/auth'); // import signup validator
+const { runValidation } = require('../validators'); // import validator index
+
 const router = express.Router();
 
-router.get('/signup', signup);
+router.post('/signup', userSignupValidator , runValidation , signup);
 
 module.exports = router;
