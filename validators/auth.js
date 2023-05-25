20cd407826dc64192e6aeb365fe097832b4d2c4b
Ali Ahmad <mian41935@gmail.com>
2023-05-26 03:09:26 +0500

    Send email for account activation throw send grid #8 🚀

--- validators/auth.js
+++ validators/auth.js
@@ -1,9 +1,14 @@
-const { check } = require('express-validator');
+const {check}=require('express-validator')
 
 exports.userSignupValidator = [
-  check('name').not().isEmpty().withMessage('Name is required'),
-  check('email').isEmail().withMessage('Must be a valid email address'),
-  check('hashed_password')
-    .isLength({ min: 6 })
-    .withMessage('Password must be at least  6 characters long'),
+    check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Name is required'),
+    check('email')
+        .isEmail()
+        .withMessage('Must be a valid email address'),
+    check('hashed_password') 
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least  6 characters long')
 ];
